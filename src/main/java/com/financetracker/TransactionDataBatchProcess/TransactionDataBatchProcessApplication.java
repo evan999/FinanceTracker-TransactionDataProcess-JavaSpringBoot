418bf402ae195c03fa82c6dd0cd8735fca4c1096
evan999 <emeshberge@gmail.com>
2020-05-08 23:07:11 -0400

    Start up Spring Boot Application with downloaded dependencies. Declare datasource configuration.

---
+++ src/main/java/com/financetracker/TransactionDataBatchProcess/TransactionDataBatchProcessApplication.java
@@ -0,0 +1,13 @@
+package com.financetracker.TransactionDataBatchProcess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionDataBatchProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionDataBatchProcessApplication.class, args);
+	}
+
+}
